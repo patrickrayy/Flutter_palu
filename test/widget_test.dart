fc21de9ed58d8dd49f0c0b6974a5c8ab028c54b0
Ray <patrickrayymnd@students.unnes.ac.id>
2024-06-16 20:18:04 +0700

    chore: Remove unnecessary configuration files for Linux, macOS, and Windows platforms

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:PALU/main.dart';
+import 'package:flutter_palu/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
